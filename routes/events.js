56d7fe60cc24474e25818b284e4c69c487c3c0fe
dev_castle <luis_castle_2011@hotmai.com>
2023-01-05 00:37:04 -0500

    Backend 1.0 terminado

---
+++ routes/events.js
@@ -0,0 +1,34 @@
+/*
+Events Routes
+/api/events
+*/
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require('../middlewares/validar-campos');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require("../controllers/events");
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { isDate } = require("../helpers/isDate");
+
+const router = Router();
+
+//Todas tienes que pasar por la validación del JWT
+router.use(validarJWT);
+
+// Obtener events
+router.get('/', getEventos)
+
+// Crear un nuevo evento
+router.post('/',[
+    check('title','El título es obligatorio').not().isEmpty(),
+    check('start','Fecha de inicio es obligatoria').custom(isDate),
+    check('end','Fecha de finalización es obligatoria').custom(isDate),
+    validarCampos
+], crearEvento);
+
+// Actualizar evento
+router.put('/:id', actualizarEvento);
+
+// Borrar evento
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
